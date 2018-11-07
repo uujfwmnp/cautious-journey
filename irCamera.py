6ad029f9c89a88f72f890e500191caa1f2b90f8f
uujfwmnp <3219994+uujfwmnp@users.noreply.github.com>
2018-11-07 13:54:55 -0800

    No tabs, only spaces!
    Replaced tab code formatting with spaces.

--- irCamera.py
+++ irCamera.py
@@ -75,113 +75,113 @@ def interactiveMenu():
     pass
 
 def changePosition(activeSession,groupNum): #Change active position
-	if ir['SessionInfo']['Sessions'][activeSession]['SessionType'] in ("Practice", "Lone Qualify", "Qualify") and ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'] == None:
-		print ("There are currently no driver positions in this session. Try again later.\n\n")
-		time.sleep(1)
-	print("\nSelect New Position:")
-	print ("Pos","\t","Class","\t","Num","\t", "Driver")
-	for i in range(0, len(ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'])):
-		position = ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'][i]['Position']
-		classPos = ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'][i]['ClassPosition']
-		#carID = ir['CarIdxPosition'][position]
-		carID = ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'][i]['CarIdx']
-		driverName = ir['DriverInfo']['Drivers'][carID]['UserName']
-		carNum = ir['DriverInfo']['Drivers'][carID]['CarNumberRaw']
-		TeamName = ir['DriverInfo']['Drivers'][carID]['TeamName']
-		TeamID = ir['DriverInfo']['Drivers'][carID]['TeamID']
-		if (TeamID == "0"):
-			print (position, "\t", carNum, "\t" , driverName)
-		else:
-			print (position, "\t", classPos+1, "\t" , carNum, "\t" , TeamName)
-	newPos = input("\tEnter Position Number > ")
-	if (newPos == "") or (not re.match("(\d+)",newPos)): #little bit of regex to kill the script if anything other than 0-9 is entered
-		print("\nReturning to Main Menu..\n")
-		time.sleep(1)
-	else:
-		newCamera = int(groupNum)
-		newPosition = int(newPos)
-		ir.cam_switch_pos(newPosition,newCamera,0)
-		print("ir.cam_switch_pos(",newPosition,",",newCamera,",0)")
-		print('\nDone. Returning to menu...\n\n')
-		time.sleep(1)
-		del position, carID, driverName, newPos, newCamera, newPosition
+    if ir['SessionInfo']['Sessions'][activeSession]['SessionType'] in ("Practice", "Lone Qualify", "Qualify") and ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'] == None:
+        print ("There are currently no driver positions in this session. Try again later.\n\n")
+        time.sleep(1)
+    print("\nSelect New Position:")
+    print ("Pos","\t","Class","\t","Num","\t", "Driver")
+    for i in range(0, len(ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'])):
+        position = ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'][i]['Position']
+        classPos = ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'][i]['ClassPosition']
+        #carID = ir['CarIdxPosition'][position]
+        carID = ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'][i]['CarIdx']
+        driverName = ir['DriverInfo']['Drivers'][carID]['UserName']
+        carNum = ir['DriverInfo']['Drivers'][carID]['CarNumberRaw']
+        TeamName = ir['DriverInfo']['Drivers'][carID]['TeamName']
+        TeamID = ir['DriverInfo']['Drivers'][carID]['TeamID']
+        if (TeamID == "0"):
+            print (position, "\t", carNum, "\t" , driverName)
+        else:
+            print (position, "\t", classPos+1, "\t" , carNum, "\t" , TeamName)
+    newPos = input("\tEnter Position Number > ")
+    if (newPos == "") or (not re.match("(\d+)",newPos)): #little bit of regex to kill the script if anything other than 0-9 is entered
+        print("\nReturning to Main Menu..\n")
+        time.sleep(1)
+    else:
+        newCamera = int(groupNum)
+        newPosition = int(newPos)
+        ir.cam_switch_pos(newPosition,newCamera,0)
+        print("ir.cam_switch_pos(",newPosition,",",newCamera,",0)")
+        print('\nDone. Returning to menu...\n\n')
+        time.sleep(1)
+        del position, carID, driverName, newPos, newCamera, newPosition
 
 def changeTeam(activeSession,groupNum,activeCam): #Change active team (WORK IN PROGRESS)
-	if ir['SessionInfo']['Sessions'][activeSession]['SessionType'] in ("Practice", "Lone Qualify", "Qualify") and ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'] == None:
-		print ("There are currently no driver positions in this session. Try again later.\n\n")
-		time.sleep(1)
-	TeamID = ir['DriverInfo']['Drivers'][1]['TeamID']			#Car 0 is always the Pace Car, so we look at Car 1.
-	if (TeamID == 0):	#If TeamID of the 1st place car is 0, then this isn't a team race.
-		print ("\nThis isn't a team race!\n\nReturning to Main Menu..\n")
-		del TeamID
-		time.sleep(1)
-	else:
-		print (ir['DriverInfo']['Drivers'][1]['UserName'], TeamID)
-		print("\nSelect New Team:")
-		print ("CarNum\t", "Team Name\t")
-		for i in range(1, len(ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'])):
-		#for i in range(1, len(ir['DriverInfo']['Drivers'])):
-			carID = ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'][i]['CarIdx']
-			posCarNum = ir['DriverInfo']['Drivers'][i]['CarNumberRaw']
-			TeamID = ir['DriverInfo']['Drivers'][carID]['TeamID']
-			TeamName = ir['DriverInfo']['Drivers'][i]['TeamName']
-			PaceCar = ir['DriverInfo']['Drivers'][i]['CarIsPaceCar']
-			print (posCarNum, "\t", TeamName)
-		changeTeam = input("\tEnter Team Car Number > ")
-		if (changeTeam == "") or (not re.match("(\d+)",changeTeam)): #little bit of regex to kill the script if anything other than 0-9 is entered
-			print("\nReturning to Main Menu..\n")
-			time.sleep(1)
-		else:
-			print("ir.cam_switch_num(",changeTeam,",",groupNum,", 0)")
-			ir.cam_switch_num(changeTeam, groupNum, 1)
-			print('\nDone. Returning to menu...\n\n')
-			time.sleep(1)
+    if ir['SessionInfo']['Sessions'][activeSession]['SessionType'] in ("Practice", "Lone Qualify", "Qualify") and ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'] == None:
+        print ("There are currently no driver positions in this session. Try again later.\n\n")
+        time.sleep(1)
+    TeamID = ir['DriverInfo']['Drivers'][1]['TeamID']           #Car 0 is always the Pace Car, so we look at Car 1.
+    if (TeamID == 0):   #If TeamID of the 1st place car is 0, then this isn't a team race.
+        print ("\nThis isn't a team race!\n\nReturning to Main Menu..\n")
+        del TeamID
+        time.sleep(1)
+    else:
+        print (ir['DriverInfo']['Drivers'][1]['UserName'], TeamID)
+        print("\nSelect New Team:")
+        print ("CarNum\t", "Team Name\t")
+        for i in range(1, len(ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'])):
+        #for i in range(1, len(ir['DriverInfo']['Drivers'])):
+            carID = ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'][i]['CarIdx']
+            posCarNum = ir['DriverInfo']['Drivers'][i]['CarNumberRaw']
+            TeamID = ir['DriverInfo']['Drivers'][carID]['TeamID']
+            TeamName = ir['DriverInfo']['Drivers'][i]['TeamName']
+            PaceCar = ir['DriverInfo']['Drivers'][i]['CarIsPaceCar']
+            print (posCarNum, "\t", TeamName)
+        changeTeam = input("\tEnter Team Car Number > ")
+        if (changeTeam == "") or (not re.match("(\d+)",changeTeam)): #little bit of regex to kill the script if anything other than 0-9 is entered
+            print("\nReturning to Main Menu..\n")
+            time.sleep(1)
+        else:
+            print("ir.cam_switch_num(",changeTeam,",",groupNum,", 0)")
+            ir.cam_switch_num(changeTeam, groupNum, 1)
+            print('\nDone. Returning to menu...\n\n')
+            time.sleep(1)
 
 def changeCamera(activeSession,activeCam,activeNumber): #Change active camera, driver remains the same
-	if ir['SessionInfo']['Sessions'][activeSession]['SessionType'] in ("Practice", "Lone Qualify", "Qualify") and ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'] == None:
-		print ("There are currently no driver positions in this session. Try again later.\n\n")
-		time.sleep(1)
-	print("\nSelect New Camera:")
-	print ("Active Camera: ", ir['CameraInfo']['Groups'][activeCam]['GroupName'])
-	print ("Group\t", "Name\t")
-	for _grp in range(0, len(ir['CameraInfo']['Groups'])):
-		changeGroup = ir['CameraInfo']['Groups'][_grp]['GroupNum']
-		changeName	= ir['CameraInfo']['Groups'][_grp]['GroupName']
-		print (changeGroup, "\t", changeName)
-	changeCam = input("\tSelect New Camera > ")
-	if (changeCam == "") or (not re.match("(\d+)",changeCam)): #little bit of regex to kill the script if anything other than 0-9 is entered
-		print("\nReturning to Main Menu..\n")
-		time.sleep(1)
-	else:
-		newCamera = int(changeCam)
-		ir.cam_switch_num(activeNumber, newCamera, 1)
-		print("ir.cam_switch_num(",activeNumber,",",changeCam,",0)")
-		print('\nDone. Returning to menu...\n\n')
-		time.sleep(1)
+    if ir['SessionInfo']['Sessions'][activeSession]['SessionType'] in ("Practice", "Lone Qualify", "Qualify") and ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'] == None:
+        print ("There are currently no driver positions in this session. Try again later.\n\n")
+        time.sleep(1)
+    print("\nSelect New Camera:")
+    print ("Active Camera: ", ir['CameraInfo']['Groups'][activeCam]['GroupName'])
+    print ("Group\t", "Name\t")
+    for _grp in range(0, len(ir['CameraInfo']['Groups'])):
+        changeGroup = ir['CameraInfo']['Groups'][_grp]['GroupNum']
+        changeName  = ir['CameraInfo']['Groups'][_grp]['GroupName']
+        print (changeGroup, "\t", changeName)
+    changeCam = input("\tSelect New Camera > ")
+    if (changeCam == "") or (not re.match("(\d+)",changeCam)): #little bit of regex to kill the script if anything other than 0-9 is entered
+        print("\nReturning to Main Menu..\n")
+        time.sleep(1)
+    else:
+        newCamera = int(changeCam)
+        ir.cam_switch_num(activeNumber, newCamera, 1)
+        print("ir.cam_switch_num(",activeNumber,",",changeCam,",0)")
+        print('\nDone. Returning to menu...\n\n')
+        time.sleep(1)
 
 def changeDriver(activeSession,groupNum): #Change active driver, camera remains the same
-	if ir['SessionInfo']['Sessions'][activeSession]['SessionType'] in ("Practice", "Lone Qualify", "Qualify") and ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'] == None:
-		print ("There are currently no driver positions in this session. Try again later.\n\n")
-		time.sleep(1)
-	#Make an array out of ['DriverInfo']['Drivers'], then remove ['DriverInfo']['Drivers'][i]['IsSpectator'] = 1, and then parse that in a for loop.
-	driversRaw = ir['DriverInfo']['Drivers']
-	driversList = []
-	for i in range(0, len(driversRaw)):
-		if (driversRaw[i]['IsSpectator'] == 0):
-			driversList.append (driversRaw[i])
-	for i in range(0, len(driversList)):
-		CarNum = driversList[i]['CarNumber']
-		driverName = driversList[i]['UserName']
-		TeamName = driversList[i]['TeamName']
-		print (CarNum, "\t", driverName)
-	newDriver = input("\tEnter Car Number > ")
-	if (newDriver == "") or (not re.match("(\d+)",newDriver)): #little bit of regex to kill the script if anything other than 0-9 is entered
-		print("\nReturning to Main Menu..\n")
-		time.sleep(1)
-	else:
-		print("ir.cam_switch_num(",newDriver,",",groupNum,", 0)")
-		ir.cam_switch_num(newDriver, groupNum, 1)
-		print('\nDone. Returning to menu...\n\n')
-		time.sleep(1)
+    if ir['SessionInfo']['Sessions'][activeSession]['SessionType'] in ("Practice", "Lone Qualify", "Qualify") and ir['SessionInfo']['Sessions'][activeSession]['ResultsPositions'] == None:
+        print ("There are currently no driver positions in this session. Try again later.\n\n")
+        time.sleep(1)
+    #Make an array out of ['DriverInfo']['Drivers'], then remove ['DriverInfo']['Drivers'][i]['IsSpectator'] = 1, and then parse that in a for loop.
+    driversRaw = ir['DriverInfo']['Drivers']
+    driversList = []
+    for i in range(0, len(driversRaw)):
+        if (driversRaw[i]['IsSpectator'] == 0):
+            driversList.append (driversRaw[i])
+    for i in range(0, len(driversList)):
+        CarNum = driversList[i]['CarNumber']
+        driverName = driversList[i]['UserName']
+        TeamName = driversList[i]['TeamName']
+        print (CarNum, "\t", driverName)
+    newDriver = input("\tEnter Car Number > ")
+    if (newDriver == "") or (not re.match("(\d+)",newDriver)): #little bit of regex to kill the script if anything other than 0-9 is entered
+        print("\nReturning to Main Menu..\n")
+        time.sleep(1)
+    else:
+        print("ir.cam_switch_num(",newDriver,",",groupNum,", 0)")
+        ir.cam_switch_num(newDriver, groupNum, 1)
+        print('\nDone. Returning to menu...\n\n')
+        time.sleep(1)
 
 interactiveMenu()
